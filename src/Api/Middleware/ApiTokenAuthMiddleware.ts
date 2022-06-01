cc18f95228e347817199e3b4b364746992e2108f
Sergio <sergiazow@gmail.com>
2022-06-01 22:59:34 +0200

    feat: disable auth for GET methods

--- src/Api/Middleware/ApiTokenAuthMiddleware.ts
+++ src/Api/Middleware/ApiTokenAuthMiddleware.ts
@@ -5,8 +5,10 @@ import { ILogger } from "../../Interfaces/ILogger";
 export const ApiTokenAuthMiddleware =
   (logger: ILogger) => (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers["authorization"];
-    if (!token || token !== getEnv("API_TOKEN")) {
-      return res.status(401).send({ message: "Invalid token" });
+    if (["POST", "PUT"].includes(req.method)) {
+      if (!token || token !== getEnv("API_TOKEN")) {
+        return res.status(401).send({ message: "Invalid token" });
+      }
     }
     return next();
   };
