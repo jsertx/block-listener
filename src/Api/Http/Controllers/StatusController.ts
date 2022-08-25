1ee9962b432992a8e0fff66f32f5c7ca34d43368
Sergio <sergiazow@gmail.com>
2022-08-25 11:38:39 +0200

    feat: move /status to /

--- src/Api/Http/Controllers/StatusController.ts
+++ src/Api/Http/Controllers/StatusController.ts
@@ -1,7 +1,7 @@
 import { interfaces, controller, httpGet } from "inversify-express-utils";
 import { IApiResponse } from "../Types/Response";
 
-@controller("/status")
+@controller("/")
 export class StatusController implements interfaces.Controller {
 	@httpGet("/")
 	index(): IApiResponse<string> {
