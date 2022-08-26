e6293e972169b93e3bbade2254d851a2462da6e2
Sergio <sergiazow@gmail.com>
2022-08-26 12:48:27 +0200

    feat: add more info to status

--- src/Api/Http/Controllers/StatusController.ts
+++ src/Api/Http/Controllers/StatusController.ts
@@ -1,6 +1,10 @@
 import { inject } from "inversify";
 import { interfaces, controller, httpGet } from "inversify-express-utils";
 import { ICache } from "../../../App/Interfaces/ICache";
+import { ITokenRepository } from "../../../App/Repository/ITokenRepository";
+import { ITxRepository } from "../../../App/Repository/ITxRepository";
+import { IWalletRepository } from "../../../App/Repository/IWalletRepository";
+import { TxType } from "../../../App/Values/TxType";
 import { IConfig } from "../../../Interfaces/IConfig";
 import { IocKey } from "../../../Ioc/IocKey";
 import { StatusResponseDto } from "../Dto/StatusDto";
@@ -10,19 +14,61 @@ import { IApiResponse } from "../Types/Response";
 export class StatusController implements interfaces.Controller {
 	constructor(
 		@inject(IocKey.Config) private config: IConfig,
-		@inject(IocKey.Cache) private cache: ICache
+		@inject(IocKey.Cache) private cache: ICache,
+		@inject(IocKey.TokenRepository)
+		private tokenRepository: ITokenRepository,
+		@inject(IocKey.WalletRepository)
+		private walletRepository: IWalletRepository,
+		@inject(IocKey.TxRepository) private txRepository: ITxRepository
 	) {}
 	@httpGet("/")
 	async index(): Promise<IApiResponse<StatusResponseDto>> {
 		return {
 			success: true,
 			data: {
-				latestBlocks: await this.getLatestBlocks()
+				latestBlocks: await this.getLatestBlocks(),
+				counter: await this.getCounter()
 			}
 		};
 	}
+	private async getCounter(): Promise<StatusResponseDto["counter"]> {
+		const dexSwaps = await this.txRepository.findAll({
+			pageSize: 1,
+			page: 1,
+			where: { type: TxType.DexSwap }
+		});
+		const unknownTxs = await this.txRepository.findAll({
+			pageSize: 1,
+			page: 1,
+			where: { type: TxType.Unknown }
+		});
+		const ethTransfers = await this.txRepository.findAll({
+			pageSize: 1,
+			page: 1,
+			where: { type: TxType.EthTransfer }
+		});
+		const wallets = await this.walletRepository.findAll({
+			pageSize: 1,
+			page: 1
+		});
+		const tokens = await this.tokenRepository.findAll({
+			pageSize: 1,
+			page: 1
+		});
 
-	async getLatestBlocks(): Promise<StatusResponseDto["latestBlocks"]> {
+		return {
+			tokens: tokens.total,
+			wallets: wallets.total,
+			txs: {
+				dexSwaps: dexSwaps.total,
+				ethTransfers: ethTransfers.total,
+				unknown: unknownTxs.total
+			}
+		};
+	}
+	private async getLatestBlocks(): Promise<
+		StatusResponseDto["latestBlocks"]
+	> {
 		const latestBlocksByChain = await Promise.all(
 			this.config.enabledBlockchains.map((blockchain) =>
 				this.cache.get(`latest_block_${blockchain}`)
--- src/Api/Http/Dto/StatusDto.ts
+++ src/Api/Http/Dto/StatusDto.ts
@@ -2,4 +2,13 @@ import { BlockchainId } from "../../../Config/Blockchains";
 
 export interface StatusResponseDto {
 	latestBlocks: Partial<Record<BlockchainId, number>>;
+	counter: {
+		wallets: number;
+		txs: {
+			dexSwaps: number;
+			ethTransfers: number;
+			unknown: number;
+		};
+		tokens: number;
+	};
 }
