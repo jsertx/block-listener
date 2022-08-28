f0f8fdb7c4add72ff9f002a185160d5f2e809fec
Sergio <sergiazow@gmail.com>
2022-08-28 19:16:14 +0200

    feat: reorder entrypoint

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,24 @@ import { IocKey } from "./Ioc/IocKey";
 import { IStandaloneApps } from "./App/Interfaces/IStandaloneApps";
 import { IAdapter } from "./Interfaces/IAdapter";
 import { IExecutor } from "./Interfaces/IExecutor";
+import { getEnv } from "./App/Utils/Env";
 
 async function main() {
 	const container = await initializeContainer();
-	if (!process.env.HALT) {
-		container.getAll<IExecutor>(IocKey.Executors).forEach((listener) => {
-			listener.start();
-			listener.startRetryManager();
-		});
-		container
-			.getAll<IStandaloneApps>(IocKey.StandAloneApps)
-			.forEach((app) => {
-				app.start();
-			});
-	}
 	container
 		.getAll<IAdapter>(IocKey.Adapters)
 		.forEach((adapter) => adapter.start());
+
+	if (getEnv("HALT", "0") === "1") {
+		return;
+	}
+	container.getAll<IExecutor>(IocKey.Executors).forEach((listener) => {
+		listener.start();
+		listener.startRetryManager();
+	});
+	container.getAll<IStandaloneApps>(IocKey.StandAloneApps).forEach((app) => {
+		app.start();
+	});
 }
 
 // eslint-disable-next-line no-console
