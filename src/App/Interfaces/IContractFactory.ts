374cd98a513c8c67fb86031e3fcceda2734175bb
Sergio <sergiazow@gmail.com>
2022-08-30 23:46:29 +0200

    clean: remove unused files

--- src/App/Interfaces/IContractFactory.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "ethers";
-import { Contract } from "../Entities/Contract";
-
-export interface IContractFactory {
-	getContract(
-		address: Contract,
-		provider?: ethers.providers.BaseProvider
-	): ethers.Contract;
-}
--- src/Interfaces/ITokenService.ts
+++
@@ -1,7 +0,0 @@
-import { Token } from "../App/Entities/Token";
-import { BlockchainId } from "../App/Values/Blockchain";
-
-export interface ITokenService {
-	getWrappedToken(blockchain: BlockchainId): Promise<Token>;
-	getStableCoins(blockchain: BlockchainId): Promise<Token[]>;
-}
