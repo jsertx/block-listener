dd2ea4e5f77173bc8abce56a56213e7715656cff
Sergio <sergiazow@gmail.com>
2022-09-12 10:54:47 +0200

    feat: decrease block iteration speed

--- src/App/UseCases/BlockListener.ts
+++ src/App/UseCases/BlockListener.ts
@@ -108,7 +108,7 @@ export class BlockListener implements IStandaloneApps {
 				pendingSaveTxMsgs <
 				this.config.blockListener.maxSaveTxMessagesToHalt
 			) {
-				return sleep(10_000);
+				return sleep(1000);
 			}
 			// many messages = wait 2 min and check again
 			this.logger.log({
