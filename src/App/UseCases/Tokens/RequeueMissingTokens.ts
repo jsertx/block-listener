7c2c751b3d06c80ebb5074225210d50f44b6d439
jsertx <sergiazow@gmail.com>
2023-02-22 17:24:59 +0100

    feat: improve token iteration

--- src/App/UseCases/Tokens/RequeueMissingTokens.ts
+++ src/App/UseCases/Tokens/RequeueMissingTokens.ts
@@ -8,10 +8,8 @@ import { ITxRepository } from "../../Repository/ITxRepository";
 import { IConfig } from "../../../Interfaces/IConfig";
 import { Blockchain, BlockchainId } from "../../Values/Blockchain";
 import { noop } from "../../Utils/Misc";
-import { isSameAddress } from "../../Utils/Address";
 import { TokenDiscovered } from "../../PubSub/Messages/TokenDiscovered";
-import { CronSchedule } from "../../Types/CronSchedule";
-import Cron from "node-cron";
+import { Token } from "../../Entities/Token";
 
 @injectable()
 export class RequeueMissingTokens implements IStandaloneApps {
@@ -27,27 +25,33 @@ export class RequeueMissingTokens implements IStandaloneApps {
 	) {}
 
 	start(): void {
-		Cron.schedule(CronSchedule.EveryMinute, () => {
-			for (const blockchain of this.config.enabledBlockchains) {
-				this.processBlockchain(blockchain).then(noop).catch(noop);
-			}
-		});
+		this.processAllBlockchainsAndRepeat().then(noop).catch(noop);
+	}
+
+	async processAllBlockchainsAndRepeat() {
+		for (const blockchain of this.config.enabledBlockchains) {
+			await this.processBlockchain(blockchain);
+		}
+		setTimeout(
+			() => this.processAllBlockchainsAndRepeat().then(noop).catch(noop),
+			5 * 60 * 1000
+		);
 	}
 
 	async processBlockchain(blockchain: BlockchainId): Promise<void> {
 		const { addresses } = await this.txRepository.getAllTokensFoundInSwaps(
 			blockchain
 		);
-		const allTokens =
-			await this.tokenRepository.findTokensByBlockchainAddress({
-				addresses,
-				blockchain: new Blockchain(blockchain)
-			});
+		const allTokensByAddr: Record<string, Token> =
+			await this.tokenRepository
+				.findTokensByBlockchainAddress({
+					addresses,
+					blockchain: new Blockchain(blockchain)
+				})
+				.then((tokens) => tokens.reduce(tokensByAddrReducer, {}));
 
 		for (const address of addresses) {
-			const found = allTokens.find((token) =>
-				isSameAddress(token.address, address)
-			);
+			const found = allTokensByAddr[address.toLowerCase()];
 			if (!found) {
 				this.logger.log({
 					type: "requeue-missing-tokens",
@@ -69,4 +73,7 @@ export class RequeueMissingTokens implements IStandaloneApps {
 			}
 		}
 	}
+}
+function tokensByAddrReducer(map: Record<string, Token>, token: Token) {
+	return { ...map, [token.address.toLowerCase()]: token };
 }
